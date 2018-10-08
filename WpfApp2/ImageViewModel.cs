08181de968df68ddb9763437408f177c3351e4a2
amm0theman <ammonriley.97@gmail.com>
2018-10-08 10:12:54 -0600

    first commit

---
+++ WpfApp2/ImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class ImageViewModel
+    {
+        private int _x;
+        public int _xPosition
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+    }
+}
